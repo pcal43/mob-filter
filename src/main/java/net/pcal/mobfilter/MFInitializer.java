56ed0a1d7f0268346cff2b7b77d9db3e8135e1f3
Patrick Calahan <github@pcal.net>
2022-04-10 22:07:53 -0700

    meh

--- src/main/java/net/pcal/mobfilter/MFInitializer.java
+++ src/main/java/net/pcal/mobfilter/MFInitializer.java
@@ -1,7 +1,6 @@
 package net.pcal.mobfilter;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -14,16 +13,7 @@ public class MFInitializer implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
-        ClientLifecycleEvents.CLIENT_STARTED.register(server -> {
-            try {
-                MFService.getInstance().createDefaultConfig();
-            } catch (Exception | NoClassDefFoundError e) {
-                LOGGER.catching(Level.ERROR, e);
-                LOGGER.error("[MobFilter] failed to initialize");
-            }
-        });
-
+        MFService.getInstance().ensureConfigExists();
         ServerLifecycleEvents.SERVER_STARTING.register(server -> {
             try {
                 MFService.getInstance().loadConfig();
