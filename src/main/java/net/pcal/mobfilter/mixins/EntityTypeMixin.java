9117372cfe55865237b38096067b1fecc29469f6
Patrick Calahan <github@pcal.net>
2024-06-02 15:33:49 -0700

    checkpoint

--- src/main/java/net/pcal/mobfilter/mixins/EntityTypeMixin.java
+++ src/main/java/net/pcal/mobfilter/mixins/EntityTypeMixin.java
@@ -3,7 +3,6 @@ package net.pcal.mobfilter.mixins;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.pcal.mobfilter.MFService;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,11 +15,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EntityType.class)
 public abstract class EntityTypeMixin {
 
-    @Inject(at = @At("HEAD"), cancellable = true, method = "spawn(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/MobSpawnType);Lnet.minecraft.world.Entity")
-    public void mf_spawn(net.minecraft.server.level.ServerLevel serverLevel,
+    @Inject(at = @At("HEAD"), cancellable = true, method = "spawn(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/MobSpawnType;)Lnet/minecraft/world/entity/Entity;")
+    private void mf_spawn(net.minecraft.server.level.ServerLevel serverLevel,
                          net.minecraft.core.BlockPos blockPos,
                          net.minecraft.world.entity.MobSpawnType mobSpawnType,
-                         CallbackInfoReturnable cir) {
+                          CallbackInfoReturnable<net.minecraft.world.entity.Entity> cir) {
         if ((Object)this instanceof Mob mob) {
             if (!MFService.getInstance().isSpawnAllowed(serverLevel, mob.getType(), blockPos, mobSpawnType)) {
                 cir.setReturnValue(null);
@@ -29,15 +28,14 @@ public abstract class EntityTypeMixin {
         }
     }
 
-    @Nullable
-    @Inject(at = @At("HEAD"), cancellable = true, method = "spawn(Lnet/minecraft/server/level/ServerLevel;Ljava/util/function/Consumer;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/MobSpawnType;Z;Z);Lnet.minecraft.world.Entity")
-    public void mf_spawn(net.minecraft.server.level.ServerLevel serverLevel,
+    @Inject(at = @At("HEAD"), cancellable = true, method = "spawn(Lnet/minecraft/server/level/ServerLevel;Ljava/util/function/Consumer;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/MobSpawnType;ZZ)Lnet/minecraft/world/entity/Entity;")
+    private void mf_spawn(net.minecraft.server.level.ServerLevel serverLevel,
                          java.util.function.Consumer<?> consumer,
                          net.minecraft.core.BlockPos blockPos,
                          net.minecraft.world.entity.MobSpawnType mobSpawnType,
                          boolean bl,
                          boolean bl2,
-                         CallbackInfoReturnable cir) {
+                         CallbackInfoReturnable<net.minecraft.world.entity.Entity> cir) {
         if ((Object)this instanceof Mob mob) {
             if (!MFService.getInstance().isSpawnAllowed(serverLevel, mob.getType(), mob.blockPosition(), mobSpawnType)) {
                 cir.setReturnValue(null);
@@ -46,3 +44,4 @@ public abstract class EntityTypeMixin {
         }
     }
 }
+
