dbbfed72271a6563778a23cce361f0f11665a1bf
Patrick Calahan <github@pcal.net>
2024-06-02 21:01:47 -0700

    checkpoint

--- src/main/java/net/pcal/mobfilter/mixins/EntityTypeMixin.java
+++ src/main/java/net/pcal/mobfilter/mixins/EntityTypeMixin.java
@@ -1,12 +1,14 @@
 package net.pcal.mobfilter.mixins;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.pcal.mobfilter.MFMixinBodies;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,8 +31,9 @@ public abstract class EntityTypeMixin {
         MFMixinBodies.EntityTypeMixin_spawn((EntityType<? extends Mob>) (Object) this, serverLevel, blockPos, mobSpawnType, cir);
     }
 
-    @Inject(at = @At("HEAD"), cancellable = true, method = "spawn(Lnet/minecraft/server/level/ServerLevel;Ljava/util/function/Consumer;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/MobSpawnType;ZZ)Lnet/minecraft/world/entity/Entity;")
+    @Inject(at = @At("HEAD"), cancellable = true, method = "spawn(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/nbt/CompoundTag;Ljava/util/function/Consumer;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/MobSpawnType;ZZ)Lnet/minecraft/world/entity/Entity;")
     private void mf_spawn(ServerLevel serverLevel,
+                          CompoundTag ignored9,
                           Consumer<?> ignored0,
                           BlockPos blockPos,
                           MobSpawnType mobSpawnType,
