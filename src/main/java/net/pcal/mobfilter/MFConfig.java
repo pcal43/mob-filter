9fc33f5bbfa23828a2ac182a6fcc4d313192f21b
Patrick Calahan <github@pcal.net>
2024-09-07 14:24:57 -0700

    checkpoint

--- src/main/java/net/pcal/mobfilter/MFConfig.java
+++ src/main/java/net/pcal/mobfilter/MFConfig.java
@@ -3,7 +3,6 @@ package net.pcal.mobfilter;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
 
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.MobSpawnType;
