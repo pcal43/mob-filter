80c4b8b8b57093e5c2201d87a6bdb78a8a718c76
Patrick Calahan <github@pcal.net>
2025-01-02 21:32:09 -0800

    checkpoint

---
+++ src/main/java/net/pcal/mobfilter/MFClientInitializer.java
@@ -0,0 +1,25 @@
+package net.pcal.mobfilter;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.network.chat.Component;
+
+import static net.minecraft.ChatFormatting.RED;
+
+public class MFClientInitializer implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            if (client.player != null) {
+                String configError = MFService.getInstance().getConfigError();
+                if (configError != null) {
+                    client.player.displayClientMessage(Component.literal(
+                            "Mob Filter has been disabled due to an error in config/mobfilter.json5:\n" +
+                                    configError + "\n" +
+                                    "See log for details.").withStyle(RED), false);
+                }
+            }
+        });
+    }
+}
