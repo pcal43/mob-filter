31546170b04fb482d091acf8c4023d8ab8b3d38e
Nicolas Klenert <klenert.nicolas@gmail.com>
2025-01-24 18:15:05 +0100

    Backport fail-loudly-behaviour on configuration typos to minecraft 1.20.1

--- src/main/java/net/pcal/mobfilter/MFConfig.java
+++ src/main/java/net/pcal/mobfilter/MFConfig.java
@@ -2,6 +2,9 @@ package net.pcal.mobfilter;
 
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.MobSpawnType;
 import org.yaml.snakeyaml.LoaderOptions;
@@ -10,6 +13,7 @@ import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -26,8 +30,22 @@ public class MFConfig {
 
     static Configuration loadFromJson(final InputStream in) throws IOException {
         final String rawJson = new String(in.readAllBytes(), StandardCharsets.UTF_8);
-        final Gson gson = new Gson();
-        return gson.fromJson(rawJson, Configuration.class);
+        final Gson gson = new GsonBuilder().setLenient().create();
+        class TypoCatchingJsonReader extends JsonReader {
+            public TypoCatchingJsonReader(StringReader in) {
+                super(in);
+                super.setLenient(true);
+            }
+
+            @Override
+            public void skipValue()  {
+                // GSon calls this to silently ignore json keys that don't bind to anything.  People then get
+                // confused about why their configuration isn't fully working.  So here we just fail loudly instead.
+                // Note we don't throw IOException because GSon tries to handle that in ways that obscure the message.
+                throw new RuntimeException("Unexpected configuration names at: "+this.getPath());
+            }
+        }
+        return gson.fromJson(new TypoCatchingJsonReader(new StringReader(rawJson)), TypeToken.get(Configuration.class));
     }
 
     public static class Configuration {
