7c80b3701dbc249be3ca6d37ebcff905a80059b4
Patrick Calahan <github@pcal.net>
2025-11-14 21:22:43 -0800

    checkpoint

---
+++ forge/src/main/java/net/pcal/mobfilter/forge/ForgeMobFilterMod.java
@@ -0,0 +1,44 @@
+package net.pcal.mobfilter.forge;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.pcal.mobfilter.MobFilterService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("mobfilter")
+public class ForgeMobFilterMod {
+
+    private static final Logger LOGGER = LogManager.getLogger(ForgeMobFilterMod.class);
+
+    static {
+        LOGGER.info("[MobFilter] ForgeMobFilterMod class loaded");
+    }
+
+    public ForgeMobFilterMod(IEventBus modBus) { 
+        LOGGER.info("[MobFilter] Mod constructor called");
+        modBus.addListener(ForgeMobFilterMod::onCommonSetup);
+        NeoForge.EVENT_BUS.addListener(ForgeMobFilterMod::onServerStarting);
+    }
+
+    private static void onCommonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("[MobFilter] FMLCommonSetupEvent fired - runs on both client and server");
+        MobFilterService.get().ensureConfigFilesExist();        
+    }
+
+    private static void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("[MobFilter] ServerStartingEvent fired");
+        MobFilterService.get().ensureConfigFilesExist();
+        try {
+            MobFilterService.get().loadConfig(ForgePlatform.get());
+        } catch (Exception | NoClassDefFoundError e) {
+            LOGGER.catching(Level.ERROR, e);
+            LOGGER.error("[MobFilter] failed to initialize");
+        }
+    }
+}
+
